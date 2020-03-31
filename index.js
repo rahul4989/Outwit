f1b101e4f6d0277662b6fd38d5db2e9f584d470d
Rahul Kumar <kalrarahul955@gmail.com>
2020-04-01 01:40:29 +0530

    make it better

--- index.js
+++ index.js
@@ -10,31 +10,71 @@ app.get('/',(req,res)=>{
   res.render('index')
 })
 users=[];
+pusers={};
+sol=[];
+var total=0;
 io.on('connection',(socket)=>{
   console.log('a user connected');
   socket.on('setUsername',(data)=>{
-    if(users.indexOf(data)>-1){
+
+    if((Object.values(pusers)).indexOf(data)>-1){
       socket.emit('userExists',data + ' username is already taken!Try some other username');
     }
     else{
       users.push(data);
+      //console.log(data+"data");
+      pusers[socket.id]=data;
+      //console.log(Object.values(pusers));
+      //console.log(pusers);
       socket.emit('userSet',{username:data});
+
     }
+  })
+  socket.on('onlinepeeps',(data)=>{
+    io.sockets.emit('onlinepeople',{users:Object.values(pusers)})
   })
   socket.on('msg', function(data) {
       //Send message to everyone
       io.sockets.emit('newmsg', data);
    })
    socket.on('sendans',(data)=>{
+console.log(data);
+obj={
+  username:data.user,
+  answer:data.answer,
+  count:0
+}
+sol.push(obj);
 
-     io.sockets.emit('recievedans',{answer:data});
+     io.sockets.emit('recievedans',{answer:data.answer});
    })
    socket.on('sendques',(data)=>{
+     sol=[]
+     total=0;
      io.sockets.emit('recievedques',data)
    })
    socket.on('userAnswerSubmit',(data)=>{
+     var p=sol.findIndex(x=>x.answer==data.answer);
+     sol[p].count++;
+     total++;
+     console.log(sol);
      io.sockets.emit('userAnswerrecieved',data);
    })
+   socket.on('checkresult',(data)=>{
+   var ok=(Math.max.apply(Math, sol.map(function(obj) { return obj.count; })))
+   if(total==sol.length){
+     var index=sol.findIndex(x=>x.count==ok);
+     var k=sol[index];
+     io.sockets.emit('finalres',k);
+   }
+   })
+   socket.on('disconnect', function(){
+    console.log('user ' + pusers[socket.id] + ' disconnected');
+
+    delete pusers[socket.id];
+    io.sockets.emit('onlinepeople',{users:Object.values(pusers)})
+    console.log(Object.values(pusers))
+  });
 })
 var port= process.env.PORT || 3000;
 server.listen(port,()=>{
--- public/custom.js
+++ public/custom.js
@@ -9,6 +9,7 @@
      })
      socket.on('userSet',(data)=>{
        user=data.username;
+       console.log(data)
        document.getElementById('buttona').innerHTML='';
        document.getElementById('instructions').innerHTML='';
        document.getElementById('one1').innerHTML='<div class="input-group">\
@@ -18,12 +19,70 @@
   Send!</button></span></div>\
         <div id = "message-container"></div>';
 
-      document.getElementById('two2').innerHTML='<button type="button"class="btn btn-outline-primary btn-lg btn-block" name="button" onclick="startGame()">Start!</button>';
+      document.getElementById('two2').innerHTML='<button type="button"class="btn btn-outline-primary btn-lg btn-block" name="button" onclick="startGame()">Start!</button>\
+      <label for="options">Choose a Category:</label>\
+      <select id="optionss">\
+       <option value="11">Films</option>\
+       <option value="12">Music</option>\
+       <option value="14">Television</option>\
+       <option value="18">Computers</option>\
+       <option value="21">Sports</option>\
+       <option value="26">Celebrities</option>\
+       <option value="23">History</option>\
+       <option value="24">Politics</option>\
+       <option value="10">Books</option>\
+       <option value="9" selected>General Knowledge</option>\
+      </select>\
+      <label for="optionsss">Difficulty:</label>\
+      <select id="doptions">\
+      <option value="easy">Easy</option>\
+      <option value="medium">Medium</option>\
+      <option value="hard">hard</option>\
+      </select>';
+          document.getElementById('online1').innerHTML='Online Users';
+      socket.emit('onlinepeeps',data)
+
+
+
+     })
+     socket.on('onlinepeople',(data)=>{
+       if(user){
+       var people=data.users;
+       console.log(people[0]);
+       document.getElementById('onlinepeople').innerHTML='';
+       for(var i=0;i<people.length;i++){
+         var ul = document.getElementById("onlinepeople");
+   var li = document.createElement("li");
+   li.appendChild(document.createTextNode(people[i]));
+   ul.appendChild(li);
+       }
+     }
+
      })
      socket.on('recievedques',(data)=>{
        console.log(data);
        if(user!==null){
+         document.getElementById('popup').innerHTML='';
        document.getElementById('two2').innerHTML='<button type="button" class="btn btn-outline-primary btn-lg btn-block" name="button" onclick="startGame()">Start!</button>\
+       <label for="options">Choose a Category:</label>\
+       <select id="optionss">\
+        <option value="11">Films</option>\
+        <option value="12">Music</option>\
+        <option value="14">Television</option>\
+        <option value="18">Computers</option>\
+        <option value="21">Sports</option>\
+        <option value="26">Celebrities</option>\
+        <option value="23">History</option>\
+        <option value="24">Politics</option>\
+        <option value="10">Books</option>\
+        <option value="9" selected>General Knowledge</option>\
+        </select>\
+        <label for="optionsss">Difficulty:</label>\
+        <select id="doptions">\
+        <option value="easy">Easy</option>\
+        <option value="medium">Medium</option>\
+        <option value="hard">hard</option>\
+        </select>\
        <p class="lead">'+data.ques+'</p>\
        <div class="input-group">\
        <input id="answer" class="form-control"type="text" placeholder="Your Answer" name="option"/>\
@@ -53,23 +112,24 @@
        li.className = 'list-group-item';
 li.appendChild(document.createTextNode(data.username+" Selected "+data.answer+" as their answer"));
 ul.appendChild(li);
+socket.emit('checkresult',data);
     })
       socket.on('recievedans',(data)=>{
-        console.log(data.answer.answer)
+        console.log(data.answer)
         if(user){
 
 console.log(user);
   var radiobox = document.createElement('input');
   radiobox.type = 'radio';
   radiobox.name='ans';
-  radiobox.id=data.answer.answer;
-  radiobox.value = data.answer.answer;
+  radiobox.id=data.answer;
+  radiobox.value = data.answer;
   radiobox.onclick="handle(this)";
 
 var label=document.createElement('label')
 label.htmlFor=data.answer.answer;
 
-var d=data.answer.answer;
+var d=data.answer;
 var description=document.createTextNode(d);
 label.appendChild(description);
 
@@ -82,12 +142,21 @@ label.appendChild(description);
   container.appendChild(newline);
 }
 
-
-
+socket.on('onlineuse',(data)=>{
+  console.log(data)
+})
+socket.on('finalres',(data)=>{
+  document.getElementById('popup').innerHTML='<button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Winner?</button>';
+  document.getElementById('winnername').innerHTML=data.username + ' for the answer '+ data.answer;
+})
 
    })
    function startGame(){
-     fetch('https://opentdb.com/api.php?amount=1&type=multiple')
+     window.localStorage.removeItem('answer');
+     var x=document.getElementById('optionss').value;
+     var y=document.getElementById('doptions').value;
+     console.log(x);
+     fetch('https://opentdb.com/api.php?amount=1&category='+x+'&difficulty='+y+'&type=multiple')
     .then((response) => {
       return response.json();
     })
@@ -102,8 +171,12 @@ label.appendChild(description);
      if(answer){
      document.getElementById('answer').value='';
      document.getElementById('answer').disabled=true;
-
-     socket.emit('sendans',{answer:answer});
+     window.localStorage.setItem('answer', answer);
+     var data={
+       answer:answer,
+       user:user
+     }
+     socket.emit('sendans',data);
    }
    }
  function display(){
@@ -111,12 +184,16 @@ label.appendChild(description);
                'input[name="ans"]:checked');
 
            if(checkRadio != null) {
+             var x=window.localStorage.getItem('answer');
 
+                  if(x==checkRadio.value){
+                    alert('you cant choose your own answer')
+                  }else{
                    console.log(checkRadio.value);
                    console.log(user);
                   document.getElementById('submitbutton').disabled=true;
                    socket.emit('userAnswerSubmit',{answer:checkRadio.value,username:user})
-
+}
            }
            else {
                console.log('no value selected')
